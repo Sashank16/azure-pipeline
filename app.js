576466bb5bb9444d89967fed7ed0185b39e22d86
unknown <KAKARAPARTY SASHANK KUMAR>
2024-11-21 15:12:54 +0530

    updated password generator name

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Password Generator</title>
+            <title>Password Generator 1.0</title>
             <style>
                 /* Basic reset */
                 * {
